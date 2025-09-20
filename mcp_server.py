2a8b6137de0bf97934b1572fd14cbcd204b100d3
NabidKabir <nabid.kabir312@gmail.com>
2025-09-20 19:33:45 -0400

    updated mcpserver

--- mcp_server.py
+++ mcp_server.py
@@ -9,6 +9,32 @@ load_dotenv()
 
 recycle_mcp = FastMCP("Recycling_Server")
 
+@mcp.tool(title="Geolocator")
+def geolocate_ip(ip: str = None) -> dict:
+    """Function that locates the users location by latitude and longitude by their IP address.
+
+        Args:
+            ip (optional): IP address to check. If none, uses caller's IP.
+
+        Returns:
+            Dictionary with latitude and longitude of IP address OR error message
+    """
+
+    url = f"http://ip-api.com/json/{ip or ''}"
+
+    try:
+        response = requests.get(url, timeout=5)
+        response.raise_for_status()
+
+        geo_data = response.json()
+
+        if geo_data.get("status") != "success":
+            raise ValueError(f"Geolocation Lookup Failed {geo_data}")
+        else:
+            return {"latitude": geo_data["lat"], "longitude": geo_data["lon"]}
+    except Exception as e:
+        return {"error": str(e)}
+
 GOOGLE_API_KEY = os.environ["GOOGLE_API_KEY"]
 
 @mcp.tool(title="Places Locater")
@@ -68,3 +94,4 @@ def get_places(query: str, latitude: float, longitude: float) -> dict:
         "longitude_used": longitude,
         "results": locations
     }
+
