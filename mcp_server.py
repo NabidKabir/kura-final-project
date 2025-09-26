26636e17eb38a2bab71b113e24c42236b98ee446
NabidKabir <nabid.kabir312@gmail.com>
2025-09-25 20:22:04 -0400

    removed unused files

--- mcp_server.py
+++ mcp_server.py
@@ -57,7 +57,7 @@ async def regulation_retrieval(query: str) -> dict:
     return {"query": query, "results": [doc.page_content for doc in results]}
 
 
-web_search = TavilySearch(api_key=TAVILY_API_KEY, max_results=3)
+tavily_search = TavilySearch(api_key=TAVILY_API_KEY, max_results=3)
 
 @recycle_mcp.tool(title="Tavily Search Retreival")
 async def web_search(query: str) -> dict:
@@ -70,7 +70,7 @@ async def web_search(query: str) -> dict:
             results: results of the web search in the form of a dict
     """
 
-    search_result = await web_search.invoke(query)
+    search_result = await tavily_search.invoke(query)
 
     return search_result
 
